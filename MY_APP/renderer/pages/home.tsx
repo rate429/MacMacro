0e1b8fc6e717f851791e79ddfd85eaa61b280bd6
Rager <276484@gcloud.tisd.org>
2025-05-20 14:16:04 -0500

    update

--- MY_APP/renderer/pages/home.tsx
+++ MY_APP/renderer/pages/home.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-
+import Btn from './components/Btn'
 export default function HomePage() {
   return (
     <React.Fragment>
-      
+      <Head>
+        <title>Macro Recorder</title>
+      </Head>
+      {Btn('test','./next','test','a')}
     </React.Fragment>
   )
 }
